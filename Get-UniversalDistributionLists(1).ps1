efdf6dae24935d6f472994b0fea34be7cf7c812d
nickisonm <59802380+nickisonm@users.noreply.github.com>
2021-03-29 09:40:26 -0400

    Add files via upload

---
+++ Get-UniversalDistributionLists(1).ps1
@@ -0,0 +1,16 @@
+﻿# Script to Get the Universal Distribution Groups and their Proxy Addresses
+
+ 
+
+$OutFile = "C:\temp\UniversalDistributionGroupsInfo.csv"
+
+$OU = "OU=Distribution Lists,OU=Groups,OU=EUC,DC=us,DC=domain,DC=com"
+
+# Import Module
+Import-Module activedirectory
+ 
+# Get the Distribution Groups and filter by Universal Distribution Groups only
+$DistributionGroups = Get-ADGroup  -Filter * -SearchBase $OU -Properties proxyAddresses | ?  { ($_.Groupscope -eq "Universal")  -and ($_.GroupCategory -eq "Distribution") }
+
+# Save the Distribution Groups to file
+$DistributionGroups | select Name, GroupScope, GroupCategory, SamAccountName, @{l='ProxyAddresses';e={$_.ProxyAddresses -join ';'}} | export- $OutFile -nti
