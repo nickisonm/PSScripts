163b321236a1501d3f63c5c742d38004a6fa892b
nickisonm <59802380+nickisonm@users.noreply.github.com>
2021-04-02 09:37:24 -0400

    Add files via upload

---
+++ Get-O365YammerLicensedUsers.ps1
@@ -0,0 +1,14 @@
+﻿# Get O365 Users with Yammer Membership/License
+
+# Import Module
+Import-Module MSOnline
+
+
+# Connect to Microsoft Online Services
+
+Connect-MsolService
+
+# Get all the Users where the users are given Yammer licenses
+Get-MsolUser -All | Where-Object {($_.licenses).AccountSkuId -like "*YAMMER*"} | `
+Select-Object UserPrincipalName, DisplayName | `
+Export-Csv "C:\O365UsersWithYammerLicense.csv"  -NoTypeInformation -Encoding UTF8
