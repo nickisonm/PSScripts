817fd2daf44264a0a912d05d5379d12e1fa013fc
nickisonm <59802380+nickisonm@users.noreply.github.com>
2021-04-02 09:46:41 -0400

    Add files via upload

---
+++ Get-O365_PowerBI_LicensedUsers.ps1
@@ -0,0 +1,14 @@
+﻿# Get O365 Users with PowerBI License
+
+# Import Module
+Import-Module MSOnline
+
+
+# Connect to Microsoft Online Services
+
+Connect-MsolService
+
+# Get all the Users where the users are given Yammer licenses
+Get-MsolUser -All | Where-Object {($_.licenses).AccountSkuId -like "*PowerBI*"} | `
+Select-Object UserPrincipalName, DisplayName | `
+Export-Csv "C:\O365UsersWith_POWERBI_License.csv"  -NoTypeInformation -Encoding UTF8
